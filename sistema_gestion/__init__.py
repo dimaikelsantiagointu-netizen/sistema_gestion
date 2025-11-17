c0ad9b5a1cf81181c6f611ba3921c07df345dd2f
dimaikelsantiagointu-netizen <dimaikelsantiagointu@gmail.com>
2025-11-17 15:40:32 -0400

    Update __init__.py

--- sistema_gestion/__init__.py
+++ sistema_gestion/__init__.py
@@ -1,13 +1,1 @@
-from .base import *
 
-# Para desarrollo, usar development settings
-try:
-    from .development import *
-except ImportError:
-    pass
-
-# Para producción, usar production settings  
-try:
-    from .production import *
-except ImportError:
-    pass
