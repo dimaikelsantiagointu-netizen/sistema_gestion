3a380a09c38e8e3a0d7bdde23583f081bdf81533
Jose <ingborreroj@gmail.com>
2025-12-14 09:41:15 -0400

    refactor: Remove unnecessary comment

--- sistema_gestion/views.py
+++ sistema_gestion/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 def home_view(request):
 
-    # La función render busca 'base.html' en la carpeta 'templates' del proyecto
     return render(request, 'base.html', {})
